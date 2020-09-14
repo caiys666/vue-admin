3eb176857e0722da4751f92d669848093fa75af4
caiys666 <1255651384@qq.com>
2020-09-14 10:50:07 +0800

    构建项目,配置样式，路由，elementui

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    redirect: "login"
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: () => import("../views/login/index.vue")
+  }
+];
+
+const router = new VueRouter({
+  routes
+});
+
+export default router;
